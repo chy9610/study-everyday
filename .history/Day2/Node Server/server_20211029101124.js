7c5106db4d5df916746b4d7e3ff648091a7863e6
陈化宇 <1171193188@qq.com>
2021-10-29 10:57:03 +0800

    chore：learn Node Server

---
+++ .history/Day2/Node Server/server_20211029101124.js
@@ -0,0 +1,15 @@
+var http = require("http");
+http.createServer(function(req, res) {
+    res.writeHead(200, {
+        "Content-Type" : "text/plain;charset=utf-8" // 输出类型
+    });
+    res.write("你好");// 页面输出
+    res.end();
+}).listen(10000); // 监听端口号
+console.log("服务器启动!");
+
+
+let arr = [{name: ""}]
+
+console.log(arr)
+console.log(arr[0].name)
